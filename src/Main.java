e24036debeabff2da74d0a1f252f50de146f28df
asheefb <ashifkb80629@gmail.com>
2026-06-07 20:00:18 +0530

    Repo for All DSA Preparation.

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        findLargestNumber(array);
+    }
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    public static void findLargestNumber(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+
+        System.out.println(max);
+    }
+}
