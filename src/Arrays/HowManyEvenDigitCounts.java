02e1efdea620a33df3a650866d5d4d0e50f1ad4d
asheefb <ashifkb80629@gmail.com>
2026-06-20 16:03:04 +0530

    Added Prefix Sum Question.

--- src/Arrays/HowManyEvenDigitCounts.java
+++ src/Arrays/HowManyEvenDigitCounts.java
@@ -4,15 +4,16 @@ public class HowManyEvenDigitCounts {
     private static int evenDigitsCount(int[] nums) {
 
         int eveCount = 0;
-
         for (int j : nums) {
             int num = j;
             int count = 0;
-            //123
+            //With while
             while (num != 0) {
                 count++;
                 num = num / 10;
             }
+            //without while
+            int digits = (int) Math.log(num);
             if (count % 2 == 0)
                 eveCount++;
         }
