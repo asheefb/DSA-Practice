05d3f6577b14ae6e572fda7c6370a86ee71fe46a
asheefb <ashifkb80629@gmail.com>
2026-06-27 02:37:51 +0530

    Added Prefix Sum Questions.

---
+++ src/LeetCode/Arrays/PrefixSum/SubArraySumEqualN.java
@@ -0,0 +1,66 @@
+package LeetCode.Arrays.PrefixSum;
+
+import java.util.HashMap;
+
+public class SubArraySumEqualN {
+
+    //prefix 5%
+    public int subarraySum2(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+
+                if (sum == k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+
+                if (sum == k)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    //optimised
+    public int subarraySum3(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int prefix = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            prefix += num;
+
+            count += map.getOrDefault(prefix - k, 0);
+
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+        }
+
+        return count;
+    }
+}
