7a2d28261f185073d397d88f26295140d1ddba52
asheefb <ashifkb80629@gmail.com>
2026-06-27 02:06:34 +0530

    Added Prefix Sum Questions.

--- src/LeetCode/Arrays/PrefixSum/PivotIndex.java
+++ src/LeetCode/Arrays/PrefixSum/PivotIndex.java
@@ -1,23 +1,22 @@
 package LeetCode.Arrays.PrefixSum;
 
-import java.util.Scanner;
+/*
+https://leetcode.com/problems/find-pivot-index/
+ */
 
 public class PivotIndex {
-    public static void main(String[] args) {
-        System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
-    }
-
-    public static int pivotIndex(int[] nums) {
+    public int pivotIndex(int[] nums) {
         int n = nums.length;
+
         int[] prefix = new int[n];
         prefix[0] = nums[0];
-        for (int i = 1; i < n; i++) {
+
+        for (int i = 1; i < n; i++)
             prefix[i] = prefix[i - 1] + nums[i];
-        }
 
         for (int i = 0; i < n; i++) {
-            int rightSum = sum(prefix, i + 1, n - 1);
-            int leftSum = i == 0 ? 0 : sum(prefix, 0, i - 1);
+            int leftSum = i == 0 ? 0 : sumRange(prefix, 0, i - 1);
+            int rightSum = i == n - 1 ? 0 : sumRange(prefix, i + 1, n - 1);
 
             if (leftSum == rightSum)
                 return i;
@@ -25,10 +24,9 @@ public class PivotIndex {
         return -1;
     }
 
-    public static int sum(int[] prefix, int left, int right) {
+    public int sumRange(int[] prefix, int left, int right) {
         if (left == 0)
             return prefix[right];
-
         return prefix[right] - prefix[left - 1];
     }
 }
