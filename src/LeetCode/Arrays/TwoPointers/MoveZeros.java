e0cb82f49ce1ae764b1fee879676b9caa2f14497
asheefb <ashifkb80629@gmail.com>
2026-07-02 02:03:32 +0530

    Added Notes for Two Pointers.

--- src/LeetCode/Arrays/TwoPointers/MoveZeros.java
+++ src/LeetCode/Arrays/TwoPointers/MoveZeros.java
@@ -3,20 +3,53 @@ package LeetCode.Arrays.TwoPointers;
 import java.util.Arrays;
 
 public class MoveZeros {
+    public static void main(String[] args) {
+        moveZeroes2(new int[]{1, 0, 1});
+    }
 
-        public void moveZeroes(int[] nums) {
-            int start = 0, end = 1;
+    //    6%
+    public static void moveZeroes(int[] nums) {
+        int start = 0, end = 1;
 
-            while (end < nums.length) {
-                if (nums[start] == 0) {
-                    int temp = nums[end];
-                    nums[end] = nums[start];
-                    nums[start] = temp;
-                }
+        while (end < nums.length) {
+            if (nums[start] == 0 && nums[end] != 0) {
+                int temp = nums[end];
+                nums[end] = nums[start];
+                nums[start] = temp;
+                start++;
+                end++;
+            } else if (nums[start] == 0 && nums[end] == 0)
+                end++;
+            else {
                 start++;
                 end++;
             }
-
-            System.out.println(Arrays.toString(nums));
         }
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes2(int[] nums) {
+        int fast = 0, slow = 0;
+
+        while (fast < nums.length) {
+            if (nums[fast] != 0 && nums[slow] != 0) {
+                fast++;
+                slow++;
+                continue;
+            }
+
+            if (nums[fast] == 0)
+                fast++;
+            else {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+                fast++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
 }
