f09ec7ac854c853050c466c1ea88686bd2ec6f6e
asheefb <ashifkb80629@gmail.com>
2026-08-04 23:34:47 +0530

    Started Binary Search.

---
+++ src/LeetCode/Arrays/BinarySearch/BinarySearch.java
@@ -0,0 +1,26 @@
+package LeetCode.Arrays.BinarySearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{1, 0, 3, 5, 9, 12}, 9));
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return -1;
+    }
+
+}
