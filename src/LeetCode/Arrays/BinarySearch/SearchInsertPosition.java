7fb7ee97abc6edf1f937668ae5d07ee698479df9
asheefb <ashifkb80629@gmail.com>
2026-08-05 23:34:58 +0530

    Started Binary Search.

---
+++ src/LeetCode/Arrays/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,27 @@
+package LeetCode.Arrays.BinarySearch;
+
+/*
+https://leetcode.com/problems/search-insert-position/
+ */
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 0));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return start;
+    }
+}
