fa4cdcb377a6c4cd2e9467260f629ebff92027dd
asheefb <ashifkb80629@gmail.com>
2026-08-06 23:26:19 +0530

    Started Binary Search.

--- src/LeetCode/Arrays/BinarySearch/SearchRange.java
+++ src/LeetCode/Arrays/BinarySearch/SearchRange.java
@@ -27,4 +27,34 @@ public class SearchRange {
         }
         return res;
     }
+
+    //optimised using binary
+    public int[] searchRange2(int[] nums, int target) {
+        int first = binary(nums, target, true);
+        int last = binary(nums, target, false);
+
+        return new int[] { first, last };
+    }
+
+    private int binary(int[] nums, int target, boolean isFirst) {
+        int ans = -1;
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] < target)
+                start = mid + 1;
+            else if (nums[mid] > target)
+                end = mid - 1;
+            else {
+                ans = mid;
+                if (isFirst)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
