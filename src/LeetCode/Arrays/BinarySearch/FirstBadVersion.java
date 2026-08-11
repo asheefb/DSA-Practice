b59bec97701e7486e7bdd4f26d030a2a08ff8d54
asheefb <ashifkb80629@gmail.com>
2026-08-11 23:59:33 +0530

    Started Binary Search.

--- src/LeetCode/Arrays/BinarySearch/FirstBadVersion.java
+++ src/LeetCode/Arrays/BinarySearch/FirstBadVersion.java
@@ -1,24 +1,24 @@
-package LeetCode.Arrays.BinarySearch;
-
-/*
-https://leetcode.com/problems/first-bad-version/description/
- */
-public class FirstBadVersion {
-    /* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-    public int firstBadVersion(int n) {
-        int start = 0, end = n, ans = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (isBadVersion(mid)) {
-                ans = mid;
-                end = mid - 1;
-            } else
-                start = mid + 1;
-        }
-        return ans;
-    }
-
-}
+//package LeetCode.Arrays.BinarySearch;
+//
+///*
+//https://leetcode.com/problems/first-bad-version/description/
+// */
+//public class FirstBadVersion {
+//    /* The isBadVersion API is defined in the parent class VersionControl.
+//      boolean isBadVersion(int version); */
+//    public int firstBadVersion(int n) {
+//        int start = 0, end = n, ans = -1;
+//
+//        while (start <= end) {
+//            int mid = start + (end - start) / 2;
+//
+//            if (isBadVersion(mid)) {
+//                ans = mid;
+//                end = mid - 1;
+//            } else
+//                start = mid + 1;
+//        }
+//        return ans;
+//    }
+//
+//}
--- src/LeetCode/Arrays/BinarySearch/SplitArrayLargestSum.java
+++ src/LeetCode/Arrays/BinarySearch/SplitArrayLargestSum.java
@@ -5,4 +5,36 @@ https://leetcode.com/problems/split-array-largest-sum/description/
  */
 
 public class SplitArrayLargestSum {
+
+    public static void main(String[] args) {
+        System.out.println(splitArray(new int[]{7, 2, 5, 10, 8}, 2));
+    }
+
+    public static int splitArray(int[] nums, int k) {
+        int start = nums[0], end = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            start = Math.max(start, nums[i]);
+            end += nums[i];
+        }
+
+        while (start < end) {
+            int mid = start + (end - start) / 2, count = 1, sum = 0;
+
+            for (int num : nums) {
+                sum += num;
+                if (sum > mid) {
+                    count++;
+                    sum = num;
+                }
+            }
+
+            if (count <= k)
+                end = mid;
+            else
+                start = mid + 1;
+
+        }
+        return end;
+    }
 }
